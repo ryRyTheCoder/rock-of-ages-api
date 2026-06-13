ce557d108fa065e71b8140ba9908f9f99823cb6b
“Juan <juan_zavala24@icloud.com>
2026-06-12 19:08:07 -0500

    add health check endpoint and update deploy workflow

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -5,6 +5,7 @@ from rockapi.views import (
     register_user, login_user,
     TypeView, RockView
 )
+from rockapi.views.health_check import health_check
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'types', TypeView, 'type')
@@ -14,5 +15,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
+    path('health/', health_check),
     path('admin/', admin.site.urls),
 ]
